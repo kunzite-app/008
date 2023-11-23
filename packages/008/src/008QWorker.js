788b5c4c32bfef524a7465ce17602e61252720a0
DavidGOrtega <g.ortega.david@gmail.com>
2023-11-23 21:13:31 +0100

    008Q better queue

--- packages/008/src/008QWorker.js
+++ packages/008/src/008QWorker.js
@@ -1,33 +1,17 @@
+import PQueue from 'p-queue';
+
 import { tts } from './008Q';
 
-let BUSY = false;
-const QUEUE = [];
+const QUEUE = new PQueue({ concurrency: 2 });
 
-const process = async () => {
-  // TODO: put back busy check once error handling is fixed
-  if (!QUEUE.length) return;
-
-  try {
-    BUSY = true;
-
-    const [data] = QUEUE;
+self.addEventListener('message', async ({ data }) => {
+  console.log(`[008Q] Queuing job ${data.id}`);
+  QUEUE.add(async () => {
     const { id, audio } = data;
 
     console.log('[008Q] Transcribing...');
     const transcript = await tts({ audio });
 
     self.postMessage({ id, transcript });
-  } catch (err) {
-    console.error(err);
-  } finally {
-    QUEUE.shift();
-    BUSY = false;
-    process();
-  }
-};
-
-self.addEventListener('message', async ({ data }) => {
-  console.log(`[008Q] Queuing job ${data.id}`);
-  QUEUE.push(data);
-  process();
+  });
 });
