aadbedc78b737dcd4151b9effea615f9f5e55e20
DavidGOrtega <g.ortega.david@gmail.com>
2024-04-12 01:57:34 +0200

    3Party

--- packages/web-llm/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { ModelRecord, AppConfig } from "./config";
-export { InitProgressCallback, InitProgressReport, ChatOptions, ChatInterface, LogitProcessor, } from "./types";
-export { ChatModule, ChatRestModule, hasModelInCache } from "./chat_module";
-export { ChatWorkerHandler, ChatWorkerClient, WorkerMessage, CustomRequestParams } from "./web_worker";
-//# sourceMappingURL=index.d.ts.map
