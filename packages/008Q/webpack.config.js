be2afd2fe6d89af6684fda87594127d059106d7a
DavidGOrtega <g.ortega.david@gmail.com>
2024-04-22 17:43:50 +0200

    remove unused

--- packages/008Q/webpack.config.js
+++ packages/008Q/webpack.config.js
@@ -1,38 +1,6 @@
 const path = require("path");
 
-const webpack = require("webpack");
 const TerserPlugin = require("terser-webpack-plugin");
-
-const moduleConfig = {
-  entry: "./index.js",
-  output: {
-    globalObject: 'typeof self !== "undefined" ? self : this',
-    path: path.resolve(__dirname, "dist"),
-    filename: "008Q.js",
-    library: {
-      name: 'Q008',
-      type: 'umd'
-    },
-  },
-  mode: "production",
-  resolve: {
-    fallback: {
-      perf_hooks: false,
-    },
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-      }),
-    ],
-  },
-  pluginss: [
-    new webpack.DefinePlugin({
-      document: 'typeof document === "undefined" ? undefined : document'
-    })
-  ]
-};
 
 const globalConfig = {
   entry: "./index.js",
