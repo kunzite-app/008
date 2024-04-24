bfea1825c7682825ae2015178a447e3045f3d541
DavidGOrtega <g.ortega.david@gmail.com>
2024-04-24 16:26:45 +0200

    Worker path

---
+++ packages/008/webpack.config.js
@@ -0,0 +1,15 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const isElectron = process.env.ELECTRON_ENV === 'true';
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env
+    },
+    argv
+  );
+
+  if (isElectron) config.output.publicPath = '/';
+
+  return config;
+};
