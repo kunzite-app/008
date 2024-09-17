fc54a345897365f8472a0d970f663a4ed34862f5
DavidGOrtega <g.ortega.david@gmail.com>
2024-09-17 13:54:45 +0200

    q update

---
+++ packages/web-llm/index.d.ts
@@ -0,0 +1,9 @@
+export { ModelRecord, AppConfig, ChatOptions, EngineConfig, GenerationConfig, prebuiltAppConfig, modelVersion, modelLibURLPrefix } from "./config";
+export { InitProgressCallback, InitProgressReport, EngineInterface, LogitProcessor, } from "./types";
+export { Engine, CreateEngine, } from "./engine";
+export { hasModelInCache, deleteChatConfigInCache, deleteModelAllInfoInCache, deleteModelWasmInCache, deleteModelInCache, } from "./cache_util";
+export { EngineWorkerHandler, WebWorkerEngine, CreateWebWorkerEngine } from "./web_worker";
+export { WorkerMessage, CustomRequestParams } from "./message";
+export { ServiceWorkerEngineHandler, ServiceWorkerEngine, CreateServiceWorkerEngine, } from "./service_worker";
+export * from './openai_api_protocols/index';
+//# sourceMappingURL=index.d.ts.map
--- packages/web-llm/types.d.ts
+++ packages/web-llm/types.d.ts
@@ -1,10 +1,6 @@
-import { AppConfig, ChatConfig } from "./config";
-/**
- * Custom options that can be used to
- * override known config values.
- */
-export interface ChatOptions extends Partial<ChatConfig> {
-}
+import { AppConfig, ChatOptions, GenerationConfig } from "./config";
+import { ChatCompletionRequest, ChatCompletionRequestBase, ChatCompletionRequestStreaming, ChatCompletionRequestNonStreaming, ChatCompletion, ChatCompletionChunk } from "./openai_api_protocols/index";
+import * as API from "./openai_api_protocols/apis";
 /**
  * Report during intialization.
  */
@@ -23,7 +19,8 @@ export type InitProgressCallback = (report: InitProgressReport) => void;
 export type GenerateProgressCallback = (step: number, currentMessage: string) => void;
 /**
  * A stateful logitProcessor used to post-process logits after forwarding the input and before
- * sampling the next token.
+ * sampling the next token. If used with `GenerationConfig.logit_bias`, logit_bias is applied after
+ * `processLogits()` is called.
  */
 export interface LogitProcessor {
     /**
@@ -44,9 +41,13 @@ export interface LogitProcessor {
     resetState: () => void;
 }
 /**
- * Common interface of chat module that UI can interact with
+ * Common interface of Engine that UI can interact with
  */
-export interface ChatInterface {
+export interface EngineInterface {
+    /**
+     * An object that exposes chat-related APIs.
+     */
+    chat: API.Chat;
     /**
      * Set an initialization progress callback function
      * which reports the progress of model loading.
@@ -58,24 +59,46 @@ export interface ChatInterface {
      */
     setInitProgressCallback: (initProgressCallback: InitProgressCallback) => void;
     /**
+     * @returns The current initialization progress callback function.
+     */
+    getInitProgressCallback: () => InitProgressCallback | undefined;
+    /**
      * Reload the chat with a new model.
      *
-     * @param localIdOrUrl local_id of the model or model artifact url.
+     * @param modelId model_id of the model to load.
      * @param chatOpts Extra options to overide chat behavior.
      * @param appConfig Override the app config in this load.
      * @returns A promise when reload finishes.
      * @note This is an async function.
      */
-    reload: (localIdOrUrl: string, chatOpts?: ChatOptions, appConfig?: AppConfig) => Promise<void>;
+    reload: (modelId: string, chatOpts?: ChatOptions, appConfig?: AppConfig) => Promise<void>;
     /**
      * Generate a response for a given input.
      *
-     * @param input The input prompt.
+     * @param input The input prompt or a non-streaming ChatCompletionRequest.
      * @param progressCallback Callback that is being called to stream intermediate results.
      * @param streamInterval callback interval to call progresscallback
+     * @param genConfig Configuration for this single generation that overrides pre-existing configs.
      * @returns The final result.
+     *
+     * @note This will be deprecated soon. Please use `engine.chat.completions.create()` instead.
+     * For multi-round chatting, see `examples/multi-round-chat` on how to use
+     * `engine.chat.completions.create()` to achieve the same effect.
      */
-    generate: (input: string, progressCallback?: GenerateProgressCallback, streamInterval?: number) => Promise<string>;
+    generate: (input: string | ChatCompletionRequestNonStreaming, progressCallback?: GenerateProgressCallback, streamInterval?: number, genConfig?: GenerationConfig) => Promise<string>;
+    /**
+     * OpenAI-style API. Generate a chat completion response for the given conversation and configuration.
+     *
+     * The API is completely functional in behavior. That is, a previous request would not affect
+     * the current request's result. Thus, for multi-round chatting, users are responsible for
+     * maintaining the chat history. With that being said, as an implicit internal optimization, if we
+     * detect that the user is performing multiround chatting, we will preserve the KV cache and only
+     * prefill the new tokens.
+     */
+    chatCompletion(request: ChatCompletionRequestNonStreaming): Promise<ChatCompletion>;
+    chatCompletion(request: ChatCompletionRequestStreaming): Promise<AsyncIterable<ChatCompletionChunk>>;
+    chatCompletion(request: ChatCompletionRequestBase): Promise<AsyncIterable<ChatCompletionChunk> | ChatCompletion>;
+    chatCompletion(request: ChatCompletionRequest): Promise<AsyncIterable<ChatCompletionChunk> | ChatCompletion>;
     /**
      * @returns A text summarizing the runtime stats.
      * @note This is an async function
@@ -95,6 +118,12 @@ export interface ChatInterface {
      */
     resetChat: (keepStats?: boolean) => Promise<void>;
     /**
+     * Get the current generated response.
+     *
+     * @returns The current output message.
+     */
+    getMessage: () => Promise<string>;
+    /**
      * Returns the device's maxStorageBufferBindingSize, can be used to guess whether the device
      * has limited resources like an Android phone.
      */
@@ -110,12 +139,10 @@ export interface ChatInterface {
      * This function has side effects as the model will update its KV cache.
      *
      * @param inputIds The input tokens.
-     * @param curPos Total number of tokens processed, including the inputIds (i.e.
-     * number of tokens in KV cache plus number of tokens in inputIds).
      * @param isPrefill True if prefill, false if decode; only used for statistics.
      * @returns Next token sampled.
      * @note This is an async function.
      */
-    forwardTokensAndSample(inputIds: Array<number>, curPos: number, isPrefill: boolean): Promise<number>;
+    forwardTokensAndSample(inputIds: Array<number>, isPrefill: boolean): Promise<number>;
 }
 //# sourceMappingURL=types.d.ts.map
